e914c84863696287c3cfd672b114995300427a46
Joe Warner <fed.warner@gmail.com>
2019-03-17 21:52:48 +0000

    feat(rollup) remove unneeded type

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,9 @@
 import Fadin, { FadinConfig } from "./fadin";
 
-type FadinInit = (selector: string, options: FadinConfig) => Fadin;
-
 const fadin = (selector?: string, options: FadinConfig = {}) =>
     new Fadin({selector, noInitalScrollEvent: false, ...options})
 
 export default fadin;
 
-export { FadinConfig, Fadin, FadinInit};
+export { FadinConfig, Fadin };
 
